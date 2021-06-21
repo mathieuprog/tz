1cec26075139d146536671c38f93858e6920805e
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2021-06-21 17:12:38 +0200

    Remove unused function

--- lib/http/http_client.ex
+++ lib/http/http_client.ex
@@ -1,11 +1,3 @@
 defmodule Tz.HTTP.HTTPClient do
   @callback request(String.t(), String.t()) :: struct
-
-  import Tz.HTTP
-
-  def request(hostname, path) do
-    if get_http_client!() do
-      get_http_client!().request(hostname, path)
-    end
-  end
 end
