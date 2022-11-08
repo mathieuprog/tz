2771c6726e254dc63928277e6c5ae0affb6595cd
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2022-11-08 17:52:06 +0700

    Don't force depending on an http client

--- lib/tz/http.ex
+++ lib/tz/http.ex
@@ -3,14 +3,16 @@ defmodule Tz.HTTP do
     if http_client = Application.compile_env(:tz, :http_client) do
       http_client
     else
-      unless Code.ensure_loaded?(Mint.HTTP) do
-        raise "No HTTP client found. Add `:mint` as a dependency, or pass a custom HTTP client to the config."
+      if Code.ensure_loaded?(Mint.HTTP) do
+        Tz.HTTP.Mint.HTTPClient
       end
-
-      Tz.HTTP.Mint.HTTPClient
     end
 
   def get_http_client!() do
+    unless unquote(http_client) do
+      raise "No HTTP client found. Add `:mint` as a dependency, or pass a custom HTTP client to the config."
+    end
+
     unquote(http_client)
   end
 end
