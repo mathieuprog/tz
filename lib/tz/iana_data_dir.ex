2ef51d312ab9bb6ebf166e658a051305f0b24e87
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2022-11-27 17:49:13 +0100

    Fix support for :iana_version option

--- lib/tz/iana_data_dir.ex
+++ lib/tz/iana_data_dir.ex
@@ -1,6 +1,8 @@
 defmodule Tz.IanaDataDir do
   @moduledoc false
 
+  require Logger
+
   def dir(), do: Application.get_env(:tz, :data_dir) || to_string(:code.priv_dir(:tz))
 
   def forced_iana_version(), do: Application.get_env(:tz, :iana_version)
@@ -10,15 +12,20 @@ defmodule Tz.IanaDataDir do
       File.ls!(parent_dir)
       |> Enum.filter(&Regex.match?(~r/^tzdata20[0-9]{2}[a-z]$/, &1))
 
-    cond do
-      tz_data_dirs == [] ->
-        nil
+    if tz_data_dirs != [] do
+      latest_version = Enum.max(tz_data_dirs)
 
-      forced_version = forced_iana_version() ->
-        Enum.find(tz_data_dirs, & &1 == "tzdata#{forced_version}")
+      version =
+        if forced_version = forced_iana_version() do
+          case Enum.find(tz_data_dirs, & &1 == "tzdata#{forced_version}") do
+            nil -> Logger.warn("Tz is compiling with version #{latest_version}. Download version #{forced_version} (run `mix tz.download #{forced_version}`) and compile :tz again.")
+            version -> version
+          end
+        end
 
-      true ->
-        Enum.max(tz_data_dirs)
+      unless version do
+        latest_version
+      end
     end
   end
 
@@ -46,6 +53,7 @@ defmodule Tz.IanaDataDir do
       true ->
         if dir_name = tzdata_dir_name(to_string(:code.priv_dir(:tz))) do
           File.cp_r!(Path.join(:code.priv_dir(:tz), dir_name), Path.join(dir(), dir_name))
+          # TODO check that the right version is copied?
         else
           raise "tzdata files not found"
         end
@@ -81,7 +89,7 @@ defmodule Tz.IanaDataDir do
   end
 
   def delete_tzdata_dir(version) do
-    if (version != forced_iana_version()) do
+    if version != forced_iana_version() do
       Path.join(dir(), "tzdata#{version}")
       |> File.rm_rf!()
     end
